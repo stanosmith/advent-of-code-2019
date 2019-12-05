a1f9670c645964bd2df44ba6683fdc5fb2953027
Stan Smith <choicedesign@gmail.com>
2019-12-04 23:04:25 -0700

    Make the Intcode program work

--- day02/index.js
+++ day02/index.js
@@ -11,16 +11,31 @@ async function getInput() {
 getInput()
   .then(res => {
     const integersList = res.split(',').map(value => parseInt(value));
+
     // Opcodes are (groups of 4) indexes 0-3, 4-7, 8-11, etc.
     integersList.forEach((integer, index) => {
       const isPositionZero = index % 4 === 0;
-      console.log(isPositionZero);
+      // console.log(isPositionZero);
       if (isPositionZero) {
         // Position Zero - (1) Add, (2) Multiply, (99) End
         console.log(integer);
+        if (integer !== 99 && integer !== 0) {
+          const inputOne = integersList[index + 1];
+          const inputTwo = integersList[index + 2];
+          const outputIndex = integersList[index + 3];
+          let result = 0;
+          console.log(inputOne, inputTwo, outputIndex);
+          if (integer === 1) {
+            result = inputOne + inputTwo;
+          } else if (integer === 2) {
+            result = inputOne * inputTwo;
+          }
+          integersList[outputIndex] = result;
+        }
       }
     });
     // console.log(JSON.stringify(integersList, null, 2));
+    console.log(integersList[0]);
     console.log('Merry Christmas! 🎄');
   })
   .catch(err => console.error(err));
