c2f5bf99f0a2ffaae807585c634ab1a392539e79
Stan Smith <choicedesign@gmail.com>
2019-12-04 21:24:07 -0700

    Print Integer at position zero

--- day02/index.js
+++ day02/index.js
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2019/day/2
-const fs = require("fs").promises;
-const inputPath = "./input.txt";
+const fs = require('fs').promises;
+const inputPath = './input.txt';
 // const inputPath = "./test-input.txt";
 
 async function getInput() {
@@ -10,7 +10,17 @@ async function getInput() {
 
 getInput()
   .then(res => {
-    console.log(res);
-    console.log("Merry Christmas! 🎄");
+    const integersList = res.split(',').map(value => parseInt(value));
+    // Opcodes are (groups of 4) indexes 0-3, 4-7, 8-11, etc.
+    integersList.forEach((integer, index) => {
+      const isPositionZero = index % 4 === 0;
+      console.log(isPositionZero);
+      if (isPositionZero) {
+        // Position Zero - (1) Add, (2) Multiply, (99) End
+        console.log(integer);
+      }
+    });
+    // console.log(JSON.stringify(integersList, null, 2));
+    console.log('Merry Christmas! 🎄');
   })
   .catch(err => console.error(err));
