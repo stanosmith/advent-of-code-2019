ac2c68353fa7cb877616cc7b4907c5db9beda1b5
Stan Smith <choicedesign@gmail.com>
2019-12-07 09:08:07 -0700

    Setup autobuild with Parcel and build the lines with two.js

--- day03/index.js
+++ day03/index.js
@@ -1,7 +1,7 @@
 // https://adventofcode.com/2019/day/2
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 const inputPath = "./input.txt";
-// const inputPath = "./test-input.txt";
+// const inputPath = './test-input.txt';
 
 async function getInput() {
   const input = await fs.readFile(inputPath);
@@ -10,7 +10,15 @@ async function getInput() {
 
 getInput()
   .then(res => {
-    console.log(res);
-    console.log("Merry Christmas! 🎄");
+    // INFO: sum of absolute values of the horizontal and vertical distance
+    //  e.g. int distance = Math.abs(x1-x0) + Math.abs(y1-y0);
+    //
+    // horizontal/x = r/l
+    // vertical/y = u/d
+    //
+    const input = res.split('\n').map(wirePath => wirePath.split(','));
+    console.log(JSON.stringify(input, null, 2));
+
+    console.log('Merry Christmas! 🎄');
   })
   .catch(err => console.error(err));
